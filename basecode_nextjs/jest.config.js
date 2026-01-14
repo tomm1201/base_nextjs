332d466e43e933860ee849390f98ec7a5ea67188
tom <tomm@attcloud.org>
2026-01-14 22:00:59 +0700

    config jest

---
+++ basecode_nextjs/jest.config.js
@@ -0,0 +1,23 @@
+import nextJest from 'next/jest.js'
+
+const createJestConfig = nextJest({
+    dir: './',
+})
+
+const config = {
+    coverageProvider: 'v8',
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+    },
+    testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+    collectCoverageFrom: [
+        'src/**/*.{js,jsx,ts,tsx}',
+        '!src/**/*.d.ts',
+        '!src/**/*.stories.{js,jsx,ts,tsx}',
+        '!src/**/__tests__/**',
+    ],
+}
+
+export default createJestConfig(config)
